a9d6da4e7c332c04e1db9d552751e5155d6c1330
Lucas Silva <lucassilva080397@gmail.com>
2022-10-27 15:32:58 -0300

    feat: add file saver

--- lib/app/modules/home/submodules/meus_audios/mobx/meus_audios_store.dart
+++ lib/app/modules/home/submodules/meus_audios/mobx/meus_audios_store.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'dart:io';
 
+import 'package:audio_sextafeira/app/utils/my_snackbar.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:file_saver/file_saver.dart';
+import 'package:flutter/services.dart';
 import 'package:mobx/mobx.dart';
+import 'package:path_provider/path_provider.dart';
 part 'meus_audios_store.g.dart';
 
 class MeusAudiosStore = _MeusAudiosStoreBase with _$MeusAudiosStore;
@@ -31,9 +37,27 @@ abstract class _MeusAudiosStoreBase with Store {
 
   @action
   Future saveAudio() async {
-    String? outputFile = await FilePicker.platform.saveFile(
-      dialogTitle: 'Please select an output file:',
-      fileName: file.path,
-    );
+    try {
+      if (file.path.isNotEmpty) {
+        ByteData audioByte = await rootBundle.load(file.path);
+
+        final temp = await getTemporaryDirectory();
+
+        final name = file.path.split('/').last;
+
+        final path = '${temp.path}/$name';
+
+        final result = await FileSaver.instance
+            .saveFile(path, audioByte.buffer.asUint8List(), 'mp3');
+
+        if (result.isNotEmpty) {
+          MySnackBar(message: 'Audio salvo com sucesso!');
+        } else {
+          MySnackBar(message: 'Erro ao tentar salvar audio!');
+        }
+      }
+    } catch (e) {
+      MySnackBar(message: e.toString());
+    }
   }
 }
